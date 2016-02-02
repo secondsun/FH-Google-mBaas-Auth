2072af263e20dabc207d7c8f63ab1bdea2b3f2b7
devadmin@example.com <devadmin@example.com>
2016-02-02 01:36:36 +0000

    update file /application.js

--- application.js
+++ application.js
@@ -19,7 +19,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(mbaasExpress.fhmiddleware());
 
 app.use('/hello', require('./lib/hello.js')());
-app.use('/auth', require('./lib/hello.js')());
+app.use('/auth', require('./lib/auth.js')());
 
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
